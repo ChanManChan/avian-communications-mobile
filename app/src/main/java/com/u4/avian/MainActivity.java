0d7af6c8d4278eec265a2832c267ed3935b5559a
ChanManChan <ngopal253@gmail.com>
2022-01-01 01:06:39 +0530

    avian-init

---
+++ app/src/main/java/com/u4/avian/MainActivity.java
@@ -0,0 +1,14 @@
+package com.u4.avian;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/u4/avian/login/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.u4.avian.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.u4.avian.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
