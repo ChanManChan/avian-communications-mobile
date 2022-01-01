b85774a829e70199b16f90d42bdfe64573147b0d
ChanManChan <ngopal253@gmail.com>
2022-01-01 23:36:19 +0530

    firebase-init

--- app/src/main/java/com/u4/avian/login/LoginActivity.java
+++ app/src/main/java/com/u4/avian/login/LoginActivity.java
@@ -1,14 +1,63 @@
 package com.u4.avian.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.u4.avian.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputEditText etEmail, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+    }
+
+    public void btnLoginClick(View view) {
+        Editable emailText = etEmail.getText();
+        Editable passwordText = etPassword.getText();
+        String email, password;
+
+        if (emailText != null && passwordText != null) {
+            email = emailText.toString().trim();
+            password = passwordText.toString().trim();
+
+            if (!email.equals("") && !password.equals("")) {
+                FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Login failed: " + task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            } else {
+                if (email.equals("")) {
+                    etEmail.setError(getString(R.string.enter_email));
+                }
+
+                if (password.equals("")) {
+                    etPassword.setError(getString(R.string.enter_password));
+                }
+            }
+        }
     }
 }
