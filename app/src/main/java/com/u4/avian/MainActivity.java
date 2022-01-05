69daa0b88110890dc9b470c68bc79e58bf6fd39b
ChanManChan <ngopal253@gmail.com>
2022-01-05 19:51:28 +0530

    tab-layout-beta

--- app/src/main/java/com/u4/avian/MainActivity.java
+++ app/src/main/java/com/u4/avian/MainActivity.java
@@ -2,20 +2,60 @@ package com.u4.avian;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.material.tabs.TabLayout;
+import com.u4.avian.fragments.ChatFragment;
+import com.u4.avian.fragments.FindFragment;
+import com.u4.avian.fragments.RequestsFragment;
 import com.u4.avian.profile.ProfileActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private boolean backPressed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabMain);
+        viewPager2 = findViewById(R.id.vpMain);
+
+        setViewPager2();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (tabLayout.getSelectedTabPosition() > 0) {
+            tabLayout.selectTab(tabLayout.getTabAt(0));
+        } else {
+            if (backPressed) {
+                finishAffinity();
+            } else {
+                backPressed = true;
+                Toast.makeText(this, R.string.press_back_to_exit, Toast.LENGTH_SHORT).show();
+                android.os.Handler handler = new android.os.Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        backPressed = false;
+                    }
+                }, 2000);
+            }
+        }
     }
 
     @Override
@@ -31,5 +71,65 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(MainActivity.this, ProfileActivity.class));
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    class ViewStateAdapter extends FragmentStateAdapter {
+
+        public ViewStateAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+            super(fragmentManager, lifecycle);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position) {
+                case 0:
+                    return new ChatFragment();
+                case 1:
+                    return new RequestsFragment();
+                case 2:
+                    return new FindFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getItemCount() {
+            return tabLayout.getTabCount();
+        }
+    }
+
+    private void setViewPager2() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        ViewStateAdapter viewStateAdapter = new ViewStateAdapter(fragmentManager, getLifecycle());
+        viewPager2.setAdapter(viewStateAdapter);
+
+        tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.tab_chat));
+        tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.tab_requests));
+        tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.tab_find));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
     }
 }
